b1a514a5d05a28feb123f08ae807fddbfce15363
Lucas <lucas.teixeira34@gmail.com>
2021-01-04 23:54:02 -0300

    GET /products

---
+++ backend/src/main/java/com/devsuperior/dsdelivery/repositories/OrderRepository.java
@@ -0,0 +1,7 @@
+package com.devsuperior.dsdelivery.repositories;
+
+import com.devsuperior.dsdelivery.entities.Order;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface OrderRepository extends JpaRepository<Order, Long> {
+}
